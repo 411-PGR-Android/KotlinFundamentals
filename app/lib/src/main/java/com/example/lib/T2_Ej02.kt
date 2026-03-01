2c91b4a536987eda49c8dac3bcc0e98ddbd7ebf3
Paulina González Ramírez <pgr81678@lasallebajio.edu.mx>
2026-02-28 22:51:29 -0600

    Tarea 2 - Ejercicio 2

--- app/lib/src/main/java/com/example/lib/T2_Ej02.kt
+++ app/lib/src/main/java/com/example/lib/T2_Ej02.kt
@@ -1,2 +1,30 @@
 package com.example.lib
 
+class CuentaBancaria (val titular: String) {
+    var saldo: Double = 0.0
+
+    fun depositar(cantidad: Double){
+        saldo += cantidad
+        println("Deposito realizado, Nuevo saldo: $saldo")
+    }
+    fun retirar(cantidad: Double){
+        if (cantidad <= saldo ){
+            saldo -= cantidad
+            println("Retiro realizado. Nuevo saldo: $saldo")
+        }else{
+            println("Fondos insuficientes")
+        }
+    }
+
+    fun mostrarSaldo(){
+        println("Saldo actual de $titular: $saldo")
+    }
+}
+
+fun main () {
+    val cuenta = CuentaBancaria("Maria Garcia")
+    cuenta.depositar(1000.0)
+    cuenta.retirar(400.0)
+    cuenta.retirar(800.0) // no debe permitirlo
+    cuenta.mostrarSaldo()
+}
