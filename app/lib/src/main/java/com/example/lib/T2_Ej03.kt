7e7a7ba27465cad009e63e8a96d3b1722601f475
Paulina González Ramírez <pgr81678@lasallebajio.edu.mx>
2026-02-28 22:51:45 -0600

    Tarea 2 - Ejercicio 3

--- app/lib/src/main/java/com/example/lib/T2_Ej03.kt
+++ app/lib/src/main/java/com/example/lib/T2_Ej03.kt
@@ -1,2 +1,40 @@
 package com.example.lib
 
+open class Animal(val nombre: String, val edad: Int) {
+    open fun hacerSonido () {
+        println("Sonido genérico")
+    }
+    fun describirse() {
+        println("Soy $nombre y tengo $edad años")
+    }
+}
+
+class Perro(nombre: String, edad: Int) : Animal (nombre, edad) {
+    override fun hacerSonido() {
+        println("Gooof")
+    }
+}
+
+class Gato(nombre: String, edad: Int) : Animal (nombre, edad) {
+    override fun hacerSonido() {
+        println("Miaaauu")
+    }
+}
+
+class Vaca(nombre: String, edad: Int) : Animal (nombre, edad) {
+    override fun hacerSonido() {
+        println("Muuuu")
+    }
+}
+
+fun main () {
+    val perro = Perro ("Rex", 3)
+    perro.describirse()
+    perro.hacerSonido()
+    val gato = Gato ("Duque", 10)
+    gato.describirse()
+    gato.hacerSonido()
+    val vaca = Vaca ("kiki", 2)
+    vaca.describirse()
+    vaca.hacerSonido()
+}
